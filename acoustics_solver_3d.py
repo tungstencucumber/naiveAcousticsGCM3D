Added Gor'kov potential calculation, fixed absorptive BC. added option for reflective BC

acoustics_solver_3d.py
@@ -4,7 +4,7 @@ import os
 
 class NaiveAcousticsSolver3D:
 
-    def __init__(self, x_size, y_size, z_size, cp, rho, target_time, recording_time_step,
+    def __init__(self, x_size, y_size, z_size, cp, rho, target_time, recording_time_step, wavelength,
                  source_width, source_length_in_time, source_center_in_percents_x=50, source_center_in_percents_y=50,
                  dump_vtk=False, dump_dir="data", verbose=False):
 
@@ -31,6 +31,8 @@ class NaiveAcousticsSolver3D:
 
         self.T = 0
 
+        self.normalizing_velocity = 1000
+
         max_cp = np.max(self.cp)
         numerical_method_recommended_tau = 0.3 * min(self.hx / max_cp, self.hy / max_cp, self.hz / max_cp)
 
@@ -40,6 +42,7 @@ class NaiveAcousticsSolver3D:
         self.number_of_records = int(target_time / recording_time_step)
         self.steps_per_record = max(int(recording_time_step / numerical_method_recommended_tau), 1)
         self.tau = recording_time_step / self.steps_per_record
+        self.wavelength = wavelength
 
         if self.verbose:
             print("Doing %d data records, %d steps per record, total %d steps, time step is %f, final time %f" %
@@ -160,23 +163,34 @@ class NaiveAcousticsSolver3D:
             self.source_z_start_point:self.source_z_end_point,
             2
         ] = np.min(self.cp)
-
+        u_border = np.mgrid[
+                            -0.5*(self.source_x_end_point - self.source_x_start_point):0.5*(self.source_x_end_point - self.source_x_start_point),
+                            -0.5*(self.source_y_end_point - self.source_y_start_point):0.5*(self.source_y_end_point - self.source_y_start_point),
+                            -0.5*(self.source_z_end_point - self.source_z_start_point):0.5*(self.source_z_end_point - self.source_z_start_point)
+                            ]
+        mask = (u_border[0]**2 + u_border[1]**2 < 0.25*(self.source_x_end_point - self.source_x_start_point)**2)
         u_prev[
             self.source_x_start_point:self.source_x_end_point,
             self.source_y_start_point:self.source_y_end_point,
             self.source_z_start_point:self.source_z_end_point,
             3
-        ] = np.min(self.cp) * np.min(self.cp) * np.min(self.rho)
+        ][mask] = (np.min(self.cp) * np.min(self.cp) * np.min(self.rho) * mask)[mask]
 
         # The response recorded will be stored here
         buffer = np.zeros((self.num_points_x, self.num_points_y, self.number_of_records))
+        pressure_buffer = np.zeros((self.num_points_x, self.num_points_y, self.num_points_z))
+        pressure_squared_buffer = np.zeros((self.num_points_x, self.num_points_y, self.num_points_z))
+        velocity_buffer = np.zeros((self.num_points_x, self.num_points_y, self.num_points_z, 3))
+        velocity_norm_buffer = np.zeros((self.num_points_x, self.num_points_y, self.num_points_z))
+        gorkov_potential = np.zeros((self.num_points_x, self.num_points_y, self.num_points_z))
 
         if self.dump_vtk:
             gridToVTK(os.path.join(self.dump_dir, "u" + str(0)), self.x, self.y, self.z,
                       pointData={"vx": u_prev[:, :, :, 0].T.ravel(),
                                  "vy": u_prev[:, :, :, 1].T.ravel(),
                                  "vz": u_prev[:, :, :, 2].T.ravel(),
-                                 "p": u_prev[:, :, :, 3].T.ravel()})
+                                 "p": u_prev[:, :, :, 3].T.ravel(),
+                                 "gor'kov": gorkov_potential.T.ravel()})
 
         # Spacial steps for characterictics (they are constant, since the model is linear).
         steps = self.tau * self.cp
@@ -205,33 +219,52 @@ class NaiveAcousticsSolver3D:
                     print("Step", j)
 
                 # Three steps of 3D space-split scheme
-                u_next, inv_next = self.do_split_step(u_prev, Uz, Uz1, c_neg, c_pos, direction=2)
-                u_next, inv_next = self.do_split_step(u_next, Uy, Uy1, c_neg, c_pos, direction=1)
-                u_next, inv_next = self.do_split_step(u_next, Ux, Ux1, c_neg, c_pos, direction=0)
+                u_next, inv_next = self.do_split_step(u_prev, Uz, Uz1, c_neg, c_pos, i, direction=2)
+                u_next, inv_next = self.do_split_step(u_next, Uy, Uy1, c_neg, c_pos, i, direction=1)
+                u_next, inv_next = self.do_split_step(u_next, Ux, Ux1, c_neg, c_pos, i, direction=0)
 
                 # TODO: emitting and reflecting top border
-                # form = self.source(self.T)
-                # p0 = np.zeros(shape=(self.num_points_x, self.num_points_y))
-                # p0[self.source_x_start_point:self.source_x_end_point, self.source_y_start_point:self.source_y_end_point] = form
-                # u_next[:, :, -1, 3] = p0
-                # u_next[:, :, -1, 2] = inv_next[:, :, 0, 1] + p0 / (self.cp[:, :, 0] * self.rho[:, :, 0])
+                form = np.min(self.cp) * np.min(self.cp) * np.min(self.rho) * mask[:, :, 0] * np.cos(2 * np.pi * self.T / self.wavelength * np.min(self.cp))
+                p0 = u_next[:, :, 0, 3]
+                p0[self.source_x_start_point:self.source_x_end_point, self.source_y_start_point:self.source_y_end_point][mask[:, :, 0]] = form[mask[:, :, 0]]
+                u_next[:, :, 0, 3] = p0
+                u_next[:, :, 0, 2] = inv_next[:, :, 0, 1] + p0 / (self.cp[:, :, 0] * self.rho[:, :, 0])
 
                 buffer[:, :, -i-1] = np.copy(u_next[:, :, 1, 3])
+
+                # averaging data across all snapshots for Gor'kov potential estimation
+                pressure_buffer += np.copy(u_next[:, :, :, 3])
+                pressure_squared_buffer += u_next[:, :, :, 3]**2
+                velocity_buffer += np.copy(u_next[:, :, :, :3])
+                velocity_norm_buffer += np.linalg.norm(u_next[:, :, :, :3], axis=3)**2
+
+                # Gor'kov potential calculation (in a dimensionless form)
+                p_dimensionless = pressure_buffer / (self.rho[:,:,:] * self.cp[:,:,:] * self.normalizing_velocity * (i * self.steps_per_record + j + 1))
+                p_sq_dimensionless = pressure_squared_buffer / (self.rho[:,:,:] * self.cp[:,:,:] * self.normalizing_velocity)**2 / (i * self.steps_per_record + j + 1)
+                v_dimensionless = velocity_buffer / (self.normalizing_velocity * (i * self.steps_per_record + j + 1))
+                v_norm_dimensionless = velocity_norm_buffer / self.normalizing_velocity**2 / (i * self.steps_per_record + j + 1)
+
+                p_fluc_squared = p_sq_dimensionless - p_dimensionless**2
+                v_fluc_squared = v_norm_dimensionless - np.linalg.norm(v_dimensionless, axis=3)**2
+                p_fluc_squared[p_fluc_squared < 0.] = 0.
+                v_fluc_squared[v_fluc_squared < 0.] = 0.
+                gorkov_potential = np.sqrt(p_fluc_squared) / 3.0 - np.sqrt(v_fluc_squared) / 2.0
 
                 if self.dump_vtk:
                     gridToVTK(os.path.join(self.dump_dir, "u" + str(i + 1)), self.x, self.y, self.z,
                               pointData={"vx": u_next[:, :, :, 0].T.ravel(),
                                          "vy": u_next[:, :, :, 1].T.ravel(),
                                          "vz": u_next[:, :, :, 2].T.ravel(),
-                                         "p": u_next[:, :, :, 3].T.ravel()})
+                                         "p": u_next[:, :, :, 3].T.ravel(),
+                                         "gor'kov": gorkov_potential.T.ravel()})
 
                 u_prev = np.copy(u_next)
 
                 self.T += self.tau
 
-        return buffer
+        return buffer, gorkov_potential
 
-    def do_split_step(self, u_prev, U, U1, c_neg, c_pos, direction=-1):
+    def do_split_step(self, u_prev, U, U1, c_neg, c_pos, i, direction=-1):
         c1m, c2m, c3m = c_neg
         c1p, c2p, c3p = c_pos
 
@@ -242,8 +275,17 @@ class NaiveAcousticsSolver3D:
             u_lefts = np.pad(u_prev, ((0, 0), (1, 0), (0, 0), (0, 0)), mode='constant', constant_values=0)[:, :-1, :, :]
             u_rights = np.pad(u_prev, ((0, 0), (0, 1), (0, 0), (0, 0)), mode='constant', constant_values=0)[:, 1:, :, :]
         elif direction == 2:
+            # absorptive border conditions
             u_lefts = np.pad(u_prev, ((0, 0), (0, 0), (1, 0), (0, 0)), mode='constant', constant_values=0)[:, :, :-1, :]
+            # reflective border conditions
+            # u_lefts = np.pad(u_prev, ((0, 0), (0, 0), (1, 0), (0, 0)), mode='reflect')[:, :, :-1, :]
+            # u_lefts[:,:,0,3] = np.copy(-u_lefts[:,:,0,3])
+
+            # absorptive border conditions
             u_rights = np.pad(u_prev, ((0, 0), (0, 0), (0, 1), (0, 0)), mode='constant', constant_values=0)[:, :, 1:, :]
+            # reflective border conditions
+            # u_rights = np.pad(u_prev, ((0, 0), (0, 0), (0, 1), (0, 0)), mode='reflect')[:, :, 1:, :]
+            # u_rights[:,:,-1,3] = np.copy(-u_rights[:,:,-1,3])
         else:
             raise RuntimeError("Incorrect axis for 3D method")
 

main.py
@@ -6,23 +6,32 @@ import os
 from acoustics_solver_3d import NaiveAcousticsSolver3D
 
 # geometry sizes
-num_points_x = 100
-num_points_y = 100
-num_points_z = 30
-space_step = 10
+num_points_x = 80
+num_points_y = 80
+num_points_z = 100
+space_step = 5
 x_size = space_step * (num_points_x - 1)
 y_size = space_step * (num_points_y - 1)
 z_size = space_step * (num_points_z - 1)
 
+wavelength = 200
+dimensionless_emitter_radius = 3.858
+dimensionless_reflector_radius = 5.092
+dimensionless_reflection_surfaces_radius = 13.5
+
 # rheology limits
 rho_min = 2000
-rho_max = 3000
+rho_max = 5000
 cp_min = 2000
 cp_max = 4000
 
 # Option 1: create manually a mask for simple bright inclusion
 mask = np.zeros(shape=(num_points_x, num_points_y, num_points_z), dtype=float)
-mask[40:50, 40:50, 10:20] = 1
+relfector_shape = np.mgrid[-0.5 * num_points_x:0.5 * num_points_x,
+                    -0.5 * num_points_y:0.5 * num_points_y,
+                    0:num_points_z] * space_step
+mask += (np.sqrt(relfector_shape[0]**2 + relfector_shape[1]**2) < dimensionless_reflector_radius * wavelength / (2 * np.pi)) * \
+                    (np.sqrt(relfector_shape[0]**2 + relfector_shape[1]**2 + relfector_shape[2]**2) >= dimensionless_reflection_surfaces_radius * wavelength / (2 * np.pi))
 
 # # Option 2: get (0; 1] mask representing layered structure
 # # WARN: take a look first at https://github.com/avasyukov/quasi_marmousi and review the parameters
@@ -33,21 +42,21 @@ cp = cp_min + (cp_max - cp_min) * mask
 rho = rho_min + (rho_max - rho_min) * mask
 
 # signal recording parameters
-total_signal_recording_time = 0.225
+total_signal_recording_time = 0.9
 time_step_between_records = 0.0015
 
 # excitation pulse space size
-source_width = 50 * space_step
+source_width = dimensionless_emitter_radius * wavelength / np.pi
 source_length = 4 * time_step_between_records
 
 dump_dir = "."
 
 solver = NaiveAcousticsSolver3D(x_size, y_size, z_size, cp, rho,
-                                total_signal_recording_time, time_step_between_records,
+                                total_signal_recording_time, time_step_between_records, wavelength,
                                 source_width, source_length,
                                 source_center_in_percents_x=50, source_center_in_percents_y=50,
                                 verbose=True, dump_vtk=True, dump_dir=dump_dir)
-signal = solver.forward()
+signal, potential = solver.forward()
 print(signal.shape)
 
 gridToVTK(
@@ -62,13 +71,13 @@ gridToVTK(
 fig, ax = plt.subplots(nrows=2, ncols=1)
 
 ax[0].set_aspect('equal')
-im0 = ax[0].pcolormesh(mask[45, :, :].T)
+im0 = ax[0].pcolormesh(np.flip(mask[45, :, :].T, 0))
 fig.colorbar(im0, ax=ax[0], orientation='horizontal')
 ax[0].set_title("mask")
 
-im1 = ax[1].pcolormesh(signal[45, :, :].T)
+im1 = ax[1].contour(np.flip(potential[int(num_points_x / 2), :, :].T, 0))
 fig.colorbar(im1, ax=ax[1], orientation='horizontal')
-ax[1].set_title("signal")
+ax[1].set_title("Gor\'kov potential")
 
 plt.tight_layout()
 plt.show()